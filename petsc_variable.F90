4def3a267a20cf8a0f8bdbd99c210a56f7bf2fac
SuperLiu <1476996891@qq.com>
2018-03-30 09:17:33 +0800

    Create petsc_variable.F90

---
+++ petsc_variable.F90
@@ -0,0 +1,16 @@
+module pvariable
+  !use petscvec
+  use petscsys
+  implicit none
+#include "petsc.h"
+  Mat :: Aaaaa
+  PetscInt  :: p_size,p_rank,p_N,p_numperrow,p_i,p_j,p_nn
+  PetscScalar :: p_aprA,p_aprb
+  Mat       :: p_A 
+  Vec       :: p_b,p_x,p_x_all
+  KSP       :: P_ksp
+  PC        :: pc
+  VecScatter:: p_ctx
+  IS        :: p_from,p_to
+  PetscInt,dimension(:),allocatable :: ix
+end module
